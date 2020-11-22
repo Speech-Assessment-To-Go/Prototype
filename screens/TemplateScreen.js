a98deabb1f1f353c6a1b851b3c03c8c1a02a04db
Ramil Hinshaw <RamilHinshaw@gmail.com>
2020-11-22 12:48:31 -0600

    Update TemplateScreen.js

--- screens/TemplateScreen.js
+++ screens/TemplateScreen.js
@@ -1,48 +1,67 @@
 //------------ * IMPORT LIBS * ------------------------
 import React from 'react';
+import { Component } from 'react';
 
 import { 
   Text,
   View,
   StyleSheet,
-  Image,
-  Dimensions,
   TouchableOpacity,
-  KeyboardAvoidingView,
-  props,
   Button
 } from 'react-native';
 
-
-  //------------ * FUNCTIONS/VAR * ------------------------  
-  const ScreenContainer = ({ children }) => (
-    <View style={styles.container}>{children}</View>
-  );
-  
-  //------------ * RENDER * ------------------------
-  export const TemplateScreen = ({ navigation }) => (
-    <ScreenContainer>
-      <Text>Template Screen</Text>
-      <Button
-        title="Go Back"
-        onPress={() => navigation.goBack()} 
-      />
-
-    </ScreenContainer>
-  );
+//------------ * FUNCTIONS/VAR * ------------------------  
 
 
-// -------------- STYLE --------------------
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center"
-    },
-    button: {
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-      marginVertical: 10,
-      borderRadius: 5
-    }
-  });
+
+
+
+export class TemplateScreen extends Component
+{
+
+
+
+  // *Code Here*
+
+
+
+
+
+
+
+
+// ------------ * RENDER * ------------------------
+  render(){
+  const { navigation } = this.props;
+
+      return(
+        <View style={styles.container}>
+          <Text>Template Screen</Text>
+          <Button
+            title="Go Back"
+            onPress={() => navigation.goBack()} 
+          />
+
+        <Button title="Test" onPress={() => alert("todo!")}> </Button>
+        <Button title="Go to Template" onPress={ () => navigation.push('TemplateScreen')}> </Button>
+
+        </View>
+    );
+  }
+}
+
+
+//------------ * STYLE * ------------------------
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginVertical: 10,
+    borderRadius: 5
+  }
+});
