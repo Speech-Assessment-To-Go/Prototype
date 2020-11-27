65af5af80908280a90359f9ebb4953260f8dbb57
RamilHinshaw <RamilHinshaw@gmail.com>
2020-11-27 03:37:41 -0600

    Force Screen Orientation to Landscape

--- App.js
+++ App.js
@@ -13,6 +13,9 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
 
+import * as ScreenOrientation from 'expo-screen-orientation';
+
+
 import { Card, Button, Appbar } from 'react-native-paper';
 
 import { Student } from './Student.js';
@@ -21,18 +24,23 @@ let schools = [];
 for (var i = 0; i < 100; i++)
   schools[i] = "School " + (i+1);
 
+
 import { SchoolScreen } from './screens/SchoolScreen';
 import { TemplateScreen } from './screens/TemplateScreen';
 
 const Nav = createStackNavigator();
+
+//Force Screen Orientation to Landscape
+ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
 
 
 export default function App() {
   return (
 
     <NavigationContainer>
-      <Nav.Navigator initialRouteName="TemplateScreen">
+      <Nav.Navigator initialRouteName="SchoolScreen">
 
+        <Nav.Screen name="SchoolScreen" component = {SchoolScreen} />
         <Nav.Screen name="TemplateScreen" component = {TemplateScreen} />
 
       </Nav.Navigator>
