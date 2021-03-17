3edae0393b9b4cdca08d9f3c47080a1b0c40d185
Vincent <vincentmeyer776@hotmail.com>
2021-03-17 16:44:37 -0500

    Completion of assessmentTest.js
    assessmentTest.js now reads the assessment, displays the questions, and tallies up a score that is displayed to the terminal to the nearest percent. Also includes slight tweaks to a few other files, plus some .idea files I ironically have no idea about.

--- debug/assessmentTest.js
+++ debug/assessmentTest.js
@@ -10,10 +10,40 @@ var questions = Database.LoadQuestions();
 // console.log(questions);
 // console.log(assessments);
 
-var selectedAssessment = 1;
+// Choose the assessment to take and create an array to store the answers.
+var selectedAssessment = 4;
+var answers = [];
 
+// For each question, display the ID and text of the question.
 for (var i = 0; i < assessments[selectedAssessment].questionsID.length; i++)
 {
-    console.log(assessments[selectedAssessment].questionsID[i] + ' ' + questions[ assessments[selectedAssessment].questionsID[i]].text);
+    console.log(assessments[selectedAssessment].questionsID[i] + '. ' + questions[ assessments[selectedAssessment].questionsID[i]].text);
+    
+    // Answer each question, store its result, and output to the console if it was correct.
+    if (assessments[selectedAssessment].questionsID[i] % 2)
+    {
+        answers[i] = true;
+        console.log("Your answer was true\n");
+    }
+    else
+    {
+        answers[i] = false;
+        console.log("Your answer was false\n");
+    }
 }
 
+// Use the given answers to calculate the score as a decimal.
+var correctAnswers = 0;
+for (var i = 0; i < answers.length; i++)
+{
+    if (answers[i])
+    {
+        correctAnswers++;
+    }
+}
+var testScore = correctAnswers / answers.length;
+
+// Finally, output the score of the assessment as a rounded percentage.
+{
+    console.log("\nAssessment finished: Your score is " + Math.round(testScore * 100) + "%.");
+}
